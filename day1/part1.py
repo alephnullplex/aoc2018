6aceef80577ae7e794dbe2aa31550e3c2c269c5d
Geoff Ford <g_ford@hotmail.com>
2018-12-01 22:20:53 +1000

    Working solution for day 1!

---
+++ day1/part1.py
@@ -0,0 +1,21 @@
+with open('./input.txt') as f:
+    all = [int(x) for x in f.readlines()]
+
+    # Part 1 is to calculate the final frequency from all the variations
+    print("The total frequency is {}".format(sum(all)))
+
+    # Part 2 is to detect the first duplicate frequency when cycling throught the variations
+    # It might take many cycles of the whole list to find the first duplicate frequency
+    seen = set()
+    running_total = 0
+    duplicate = None
+    while duplicate == None:
+        for frequency in all:
+            running_total = running_total + frequency
+            if running_total in seen:
+                duplicate = running_total
+                break
+            seen.add(running_total)
+
+
+    print("Duplicate frequency is {}".format(duplicate))
